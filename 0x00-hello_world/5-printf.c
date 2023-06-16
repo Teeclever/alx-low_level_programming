557a61b053b03c82f54a79d761b8d965af3ff723
Teeclever <omotoboraomotayo@gmail.com>
2023-06-15 19:13:17 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
-#include<stdio.h>
-
+#include <stdio.h>
+/**
+ * main - A program that print line using printf
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art");
-        return (0);
+	prinf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
