2413e31c9f222f5c1c266ff52d7d37b5778992ae
Teeclever <omotoboraomotayo@gmail.com>
2023-06-19 12:14:48 -0700

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry of a program tht print a single digit number of base 10
+ * Return: always 0 (succssor)
+ */
+
+int main(void)
+{
+	int tee;
+
+	for (tee = 0; tee < 10; tee++)
+	printf("%d", tee);
+	printf("\n");
+	return (0);
+}
