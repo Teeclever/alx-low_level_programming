c83a5a8b609422ec162141d158a0813476aa42a8
Teeclever <omotoboraomotayo@gmail.com>
2023-06-19 12:39:37 -0700

    new

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entery to a program that prints all single digit number of base 10
+ * Return: alwats 0 (success)
+ */
+
+int main(void)
+{
+	int tee;
+	for (tee = 48; tee < 58; tee++)
+{
+	putchar(tee);
+}
+	putchar('\n');
+	return  (0);
+}
