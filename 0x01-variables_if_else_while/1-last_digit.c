5bead43079921681a72f4619f75b1b8996537d1b
Teeclever <omotoboraomotayo@gmail.com>
2023-06-19 09:38:29 -0700

    new

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -  The entry point to the last digit of the randoom number
+ * Return: ALways 0 (successor)
+*/
+
+int main(void)
+{
+	int n;
+
+	int t;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	t = n % 10;
+	printf("last digit of %d is %d", n, t);
+	if (t > 5)
+		printf(" and is greater then 5\n");
+	else if (t == 0)
+		printf(" and is 0\n");
+	else if (t < 6 && t != 0)
+		printf(" and is less then 6 and not 0\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - my entry point to print alphabat in lowercase
+ * Return: ALways 0 (success)
+*/
+
+int main(void)
+{
+    char te;
+    for(te ='a'; te <= 'z' te++)
+}
+{
+	putchar(\n);
+}
+	putchar('\n');
+	return (0)
