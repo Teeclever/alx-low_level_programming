80b990f251b19b3c80f7f90fd5858e0c8561ade7
Teeclever <omotoboraomotayo@gmail.com>
2023-06-19 22:10:51 -0700

    new

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int a, b, c;
 
-	for  (a = 48; a <= 58; a++)
+	for  (a = 48; a < 58; a++)
 {
-	for (b = 49; b <= 58; b++)
+	for (b = 49; b < 58; b++)
 {
-	for (c = 50; c <= 58; c++)
+	for (c = 50; c < 58; c++)
 {
-	if (c > b&&b > a)
+	if (c > b && b > a)
 {
 	putchar(a);
 	putchar(b);
