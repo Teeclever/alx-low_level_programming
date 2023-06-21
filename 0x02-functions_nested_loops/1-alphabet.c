c7741f73f68fa2530ede9cd1ad35b2033658b799
Teeclever <omotoboraomotayo@gmail.com>
2023-06-21 13:02:22 -0700

    new

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - a program tha print alphabet in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char t;
+
+	for (t = 'a'; t <= 'z'; t++)
+	{
+		_putchar(t);
+	}
+		_putchar('\n');
+}
