ecf5caecd08e8d1c97cafc1b912b6212984f643a
Teeclever <omotoboraomotayo@gmail.com>
2023-06-22 17:24:50 -0700

    new

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - A function that print the numbers 0 to 9
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9' c++)
+		{
+		_putchar(c);
+		}
+	_putchar(c);
+}
