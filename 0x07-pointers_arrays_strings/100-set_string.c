e64a2bcd5011f8232cbc962c3326c8c98c30a8b3
Teeclever <omotoboraomotayo@gmail.com>
2023-07-03 14:11:35 -0700

    new

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,27 +1,12 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_diagsums - Entry point that sets the value of a pointer to a char
- * @a: input to a pointer
- * @size: input to an integer
- * Return: Always 0 (Success)
+ * set_string - An entry that sets the value of a pointer to a char
+ * @s: pointer to pointer
+ * @to: pointer char
  */
-void print_diagsums(int *a, int size)
+
+void set_string(char **s, char *to)
 {
-	int sum1, sum2, y;
-
-	sum1 = 0;
-	sum2 = 0;
-
-	for (y = 0; y < size; y++)
-	{
-		sum1 = sum1 + a[y * size + y];
-	}
-
-	for (y = size - 1; y >= 0; y--)
-	{
-		sum2 += a[y * size + (size - y - 1)];
-	}
-
-	printf("%d, %d\n", sum1, sum2);
+	*s = to;
 }
