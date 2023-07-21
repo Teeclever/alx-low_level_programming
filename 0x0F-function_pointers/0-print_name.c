83ff7d73ec69f31dc02da93ec23d4167ed34d10f
Teeclever <omotoboraomotayo@gmail.com>
2023-07-21 12:28:23 -0700

    new

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - A program that print name using pointer to function
+ * @name: A string pointer that add strin
+ * @f: pointer to function passed
+ * Return: nothing
+ **/
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	f(name);
+}
+
