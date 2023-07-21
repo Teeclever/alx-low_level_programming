51207ac76db590219bcba02cd67c6a5dd686e0a3
Teeclever <omotoboraomotayo@gmail.com>
2023-07-21 15:58:58 -0700

    new

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
-	for (i = 0; j < size; j++)
+	for (j = 0; j < size; j++)
 	{
 		if (cmp(array[j]))
 			return (j);
