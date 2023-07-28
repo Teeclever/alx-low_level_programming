638b6d4b05c0c0092a4e360240ead4f7e5685684
Teeclever <omotoboraomotayo@gmail.com>
2023-07-27 19:59:28 -0700

    new

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  *free_list - A function that free a list
+  *@head: list to be free
+  */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+	
+	while(head)
+	{
+		current = head->next;
+		free(head->str);
+		free(head);
+		head = current;
+	}
+}
