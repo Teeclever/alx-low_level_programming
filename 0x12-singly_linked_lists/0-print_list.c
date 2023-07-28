d9a12ba871ca0d4df40fc630e88eebb7ae9d4b93
Teeclever <omotoboraomotayo@gmail.com>
2023-07-27 18:16:16 -0700

    new

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+  * print_list - A program that print all linked list element
+  * @h: pointer pointing to list to print
+  *
+  *Return: the number of printed nodes
+  */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+		while (h)
+		{
+			if (!h->str)
+			printf("[0](nil)\n");
+			else
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+			n++;
+		}
+
+	return (n);
+}
+
