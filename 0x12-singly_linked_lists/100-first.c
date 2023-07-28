efe6bfec06a102a5a4ab779a21b45b7bacf44a81
Teeclever <omotoboraomotayo@gmail.com>
2023-07-27 20:20:08 -0700

    new

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - a program that prints a sentence 
+ * function is executed
+ * Return: void
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
