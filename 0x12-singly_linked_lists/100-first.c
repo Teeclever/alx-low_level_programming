7a3cc9e4af5058ec1a24bd62a2f438a03f657540
Teeclever <omotoboraomotayo@gmail.com>
2023-07-28 18:00:08 -0700

    new

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - a program that prints a sentence 
+ * first - a program that prints a sentence
  * function is executed
  * Return: void
  */
@@ -13,4 +13,3 @@ void first(void)
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-
