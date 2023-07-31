3d43df16e8a7b0b7b430ec533440df925e161ba5
Teeclever <omotoboraomotayo@gmail.com>
2023-07-31 16:00:24 -0700

    new

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,20 +2,22 @@
 
 /**
   * print_listint - An entry that print a function
-  * @*h: a pointer
+  * @h: a pointer
+  *
   * Return: return the value of count
   */
 
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	const listint_t *current = h;
 
-	while (current != NULL)
+
+	while (h != NULL)
 	{
+
+		printf("%d\n", h->n);
 		count++;
-		printf("%d\n", current->n);
-		current = current->next;
+		h = h->next;
 	}
 	return (count);
 }
