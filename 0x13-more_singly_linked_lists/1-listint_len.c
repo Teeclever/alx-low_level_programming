429cf0fc346c4cc9d1223af42dce7ac37808827e
Teeclever <omotoboraomotayo@gmail.com>
2023-07-31 16:29:11 -0700

    new

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+
+#include "lists.h"
+
+/**
+ * listint_len - A program that returns the number of node
+ * @h: a pointer  
+ *
+ * Return: return a number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count  = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
