7d61240e5fd922b9deff65f4b027da8bfc6b8024
Teeclever <omotoboraomotayo@gmail.com>
2023-08-01 14:34:45 -0700

    new

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			current->next = temp->next;
 			temp->next = current;
 			return (current);
-            }
+		}
 		else
 			temp = temp->next;
 	}
