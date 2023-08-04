24a7102d3dea8cf773c31e14532087cb81f572e3
Teeclever <omotoboraomotayo@gmail.com>
2023-08-03 18:58:12 -0700

    new

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - A function that counts the number of bits
+ * @n: first value
+ * @m: second value
+ * 
+ * Return: the number of bits to change
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int number;
+	unsigned long int value = n ^ m;
+	    int x, print = 0;
+
+	for (x = 63; x >= 0; x--)
+	{
+		number = value >> x;
+		if (number & 1)
+			print++;
+	}
+
+	return (print);
+}
+
