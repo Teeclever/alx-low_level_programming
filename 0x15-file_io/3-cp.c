d7c69b8abf999d65577e870885db45bdebc816a4
Teeclever <omotoboraomotayo@gmail.com>
2023-08-10 16:25:11 -0700

    new

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,9 +9,9 @@
   * Return: no return
   */
 
-void errorprint(int file_place, int file_to, char *argv[])
+void errorprint(int file_from, int file_to, char *argv[])
 {
-	if (file_place == -1)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
