84684edc91bdc65b53e9405f5308e42c4215e12a
Teeclever <omotoboraomotayo@gmail.com>
2023-09-18 16:24:24 -0700

    new

---
+++ 0x18-dynamic_libraries/100-operation.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+ * add - A function that sums two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - A function that  subtracts a number from another.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - A function that  multiplies two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div -  A function hatdivides a number by another.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns the remainder when a number is divided by another.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
