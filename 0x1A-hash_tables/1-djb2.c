8b0a182e0d55db014a1d65fcdb86e4d052461aac
Teeclever <omotoboraomotayo@gmail.com>
2023-10-06 18:13:17 -0700

    new

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Hash function that uses the djb2 algorithm.
+ * @str: String to be hashed.
+ *
+ * Return: The hashed value.
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int i;
+
+	hash = 5381;
+	while ((i = *str++))
+	{
+		hash = ((hash << 5) + hash) + i;
+	}
+	return (hash);
+}
